e25fe32111ad419f31370e81499acb66a065b66f
Jashika Sethi <jashika@Jashikas-MacBook.local>
2025-04-09 01:52:00 +0530

    Media Fix

--- app/app.py
+++ app/app.py
@@ -115,7 +115,7 @@ def main():
     col1, col2 = st.columns([1,6])
     with col1:
         st.markdown('<div class="logo-container">', unsafe_allow_html=True)
-        st.image("shl-logo.png", width=80)
+        st.image("data/shl-logo.png", width=80)
         st.markdown('</div>', unsafe_allow_html=True)
     with col2:
         st.markdown(
